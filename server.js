f9e8eda5c8f4a917a27296db26612f992d6666a1
Matias <matiaslinkedin17@gmail.com>
2023-08-08 09:31:34 -0300

    validacion de Si

--- server.js
+++ server.js
@@ -11,20 +11,17 @@ app.listen(port, () => console.log("webhook is listening"));
 app.post("/webhook", async (req, res) => {
 
   try {
-
     if (req.body.entry) {
       const response = {
         nombre: req.body.entry[0].changes[0].value.contacts[0].profile.name,
         numero: req.body.entry[0].changes[0].value.messages[0].from,
         mensaje: req.body.entry[0].changes[0].value.messages[0].text.body,
-     }    
-          if (response.mensaje === "Si") {
-
-     console.log(JSON.stringify(response))
-
-      let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = req.body.entry[0].changes[0].value.messages[0].from; 
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+            }    
+      if (response.mensaje === "Si") {
+        console.log(JSON.stringify(response))
+        let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = req.body.entry[0].changes[0].value.messages[0].from; 
+        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
 
         await axios.post(
           "https://graph.facebook.com/v12.0/" + phone_number_id +"/messages?access_token=" + token,
@@ -36,39 +33,13 @@ app.post("/webhook", async (req, res) => {
           },
           headers: { "Content-Type": "application/json" },
         })
+      } else {
+        console.log("La respuesta del usuario no es un mensaje de Si")
+        return res.status(200)
       }
-      
       return res.status(200)
   }
 
-
-    // if (req.body.object) {
-    //   if (
-    //     req.body.entry &&
-    //     req.body.entry[0].changes &&
-    //     req.body.entry[0].changes[0] &&
-    //     req.body.entry[0].changes[0].value.messages &&
-    //     req.body.entry[0].changes[0].value.messages[0]
-    //   ) {
-        // let phone_number_id = 
-        //   req.body.entry[0].changes[0].value.metadata.phone_number_id;
-        // let from = req.body.entry[0].changes[0].value.messages[0].from; 
-        // let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
-        // await axios.post({
-        //   url:
-        //     "https://graph.facebook.com/v12.0/" +
-        //     phone_number_id +
-        //     "/messages?access_token=" +
-        //     token,
-        //   data: {
-        //     messaging_product: "whatsapp",
-        //     to: from,
-        //     text: { body: "Ack: " + msg_body },
-        //   },
-        //   headers: { "Content-Type": "application/json" },
-        // });
-    //   }
-    // } 
     res.status(200)
   }
   catch (error) {
