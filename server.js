b26b136261ca654ac5394dbf50803328784645a1
Matias <matiaslinkedin17@gmail.com>
2023-08-09 15:23:51 -0300

    formato de datos

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const token = process.env.WHATSAPP_TOKEN;
+// const token = process.env.WHATSAPP_TOKEN;
 const port = process.env.PORT || 3001;
 const express = require("express");
 const body_parser = require("body-parser");
-const axios = require("axios");
+// const axios = require("axios");
 const { validationPhone } = require("./controllador");
+// const { Guardian } = require("../../db");
 
 app = express().use(body_parser.json());
 
@@ -11,7 +12,7 @@ app.listen(port, () => console.log("webhook is listening"));
 
 app.post("/webhook", async (req, res) => {
   try {
-    if (req.body.entry) { 
+    if (req.body.entry) {
       const response = {
         nombre: req.body.entry[0].changes[0].value.contacts[0].profile.name,
         numero: req.body.entry[0].changes[0].value.messages[0].from,
@@ -22,17 +23,21 @@ app.post("/webhook", async (req, res) => {
 
       if (mensaje === "Si") {
         console.log(`El guardian ${nombre} va a recibir el paquete: ` + phone);
+          const phoneWithoutCountryCode = phone.slice(3)
+
+
         //aca llega la confirmacion del guardian
         return res.status(200).send("Mensaje procesado");
-      } else {
-        console.log(`Mensaje distinto de "Si" recibido de ${nombre}: ${mensaje}`);
-        return res.status(200).send("Mensaje procesado");
-      }
+      } 
+      // else {
+      //   // console.log(`Mensaje distinto de "Si" recibido de ${nombre}: ${mensaje}`);
+      //   return res.status(200).send("Mensaje procesado");
+      // }
     }
 
     res.status(200).send("Mensaje procesado");
   } catch (error) {
-    console.log(error.response);
+    console.log("Error " ,error.response);
     res.status(404).json("Error" + { error: error.response });
   }
 });
