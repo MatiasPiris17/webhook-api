6b4183fb6bbfeb270d313295e14969ee4c0f090b
Matias <matiaslinkedin17@gmail.com>
2023-08-09 10:43:04 -0300

    formato de datos

--- server.js
+++ server.js
@@ -1,60 +1,67 @@
 const token = process.env.WHATSAPP_TOKEN;
-const port =process.env.PORT || 3001
-const express = require("express")
-const body_parser = require("body-parser")
-const axios = require("axios")
-const {validationPhone} = require('./controllador')
+const port = process.env.PORT || 3001;
+const express = require("express");
+const body_parser = require("body-parser");
+const axios = require("axios");
+const { validationPhone } = require("./controllador");
 
 app = express().use(body_parser.json());
 
 app.listen(port, () => console.log("webhook is listening"));
 
 app.post("/webhook", async (req, res) => {
-
   try {
-    
     if (req.body.entry) {
       const response = {
         nombre: req.body.entry[0].changes[0].value.contacts[0].profile.name,
         numero: req.body.entry[0].changes[0].value.messages[0].from,
-        mensaje: req.body.entry[0].changes[0].value.messages[0].button.text
-      }    
-        const {numero, nombre, mensaje} = response
-        const phone = await validationPhone(numero)
+        mensaje: req.body.entry[0].changes[0].value.messages[0].button.text,
+      };
+      const { numero, nombre, mensaje } = response;
+      const phone = await validationPhone(numero);
 
-        console.log(phone)
 
-        let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
 
-        const data = {
-          messaging_product: "whatsapp",
-            to: phone,
-            text: { body: "Ack: " + mensaje },
-        } 
+      const data = {
+        messaging_product: "whatsapp",
+        to: phone,
+        text: { body: "Ack: " + mensaje },
+      };
 
-        const config = {  "Content-Type": "application/json"}
+      const config = { "Content-Type": "application/json" };
 
-        await axios.post(
-          "https://graph.facebook.com/v17.0/" + phone_number_id +"/messages?access_token=" + token,
-          data, config)
+      await axios.post(
+        "https://graph.facebook.com/v17.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data,
+        config
+      );
 
-        if (mensaje === "Si") {
-          console.log(`El guardian ${nombre} va a recibir el paquete`)
-          // const phoneguardian = phone
-          // console.log(phoneguardian)
-        }
+      if (mensaje === "Si") {
+        console.log(`El guardian ${nombre} va a recibir el paquete`);
+        //aca llegua la confirmacion del guardian
+        return res.status(200);
+      } else if (mensaje === "No") {
+        console.log(`El guardian ${nombre} NO va a recibir el paquete`);
+        return res.status(200);
+      } else {
+        console.log("El usuario mando un mensaje distinto: " + req.body);
+        return res.status(200);
+      }
 
-      return res.status(200)
+      return res.status(200);
+    }
+
+    res.status(200);
+  } catch (error) {
+    console.log(error.response);
+    res.status(404).json("Error" + { error: error.response });
   }
-
-    res.status(200)
-  }
-  catch (error) {
-    // console.log(error.response);
-    res.status(404).json("Error" + {error: error.response});
-  }
-})
-
+});
 
 app.get("/webhook", async (req, res) => {
   try {
