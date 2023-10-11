2af303176fd357d710abad2a232d8c7c16e66c08
matiaspiris17 <matiaslinkedin17@gmail.com>
2023-10-11 09:54:23 -0300

    body

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -1,7 +1,7 @@
 const { response } = require("../utils");
 
 module.exports = (req, res) => {
-  console.log(req.body)
+  console.log(JSON.stringify(req.body, null, 2));
   response(res, 200, { message: "Webhook funcionando" });
   // res.status(200).json({ message:"Webhook funcionando" })
 
