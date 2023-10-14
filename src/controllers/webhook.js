38015bf27362dd73088f65f482ad19021dab46f3
matiaspiris17 <matiaslinkedin17@gmail.com>
2023-10-14 17:04:21 -0300

    data timbre

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -1,7 +1,15 @@
 const { response } = require("../utils");
 
 module.exports = (req, res) => {
-  console.log(JSON.stringify(req.body, null, 2));
+  // console.log(JSON.stringify(req.body, null, 2));
+
+  const data = {
+    message_text: req.body.entry[0]?.changes[0]?.value?.messages[0]?.text,
+    phoneGuardian: req.body.entry[0].changes[0].value.messages[0].from,
+    id_message: req.body.entry[0].changes[0].value.messages[0].context.id,
+  }
+  console.log(data)
+
   response(res, 200, { message: "Webhook funcionando" });
   // res.status(200).json({ message:"Webhook funcionando" })
 
